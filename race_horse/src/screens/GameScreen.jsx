c7eca75849a5d2f0663efb583d732e1e1ea16f74
Gpmr5 <ganaa2z2t@gmail.com>
2025-11-27 03:07:57 +0800

    shagainii_board

--- race_horse/src/screens/GameScreen.jsx
+++ race_horse/src/screens/GameScreen.jsx
@@ -17,9 +17,23 @@ export default function GameScreen() {
         { top: '87.5%', left: '72%', transform: 'rotate(310deg)' },
         { top: '85.5%', left: '65.8%', transform: 'rotate(270deg)' },
         { top: '85.5%', left: '58.8%', transform: 'rotate(270deg)' },
-       
-        
-      
+       { top: '84.5%', left: '51.8%', transform: 'rotate(270deg)' },
+       { top: '82.5', left: '44.8%', transform: 'rotate(280deg)' },
+      { top: '83.5%', left: '37.8%', transform: 'rotate(270deg)' },
+      { top: '83.5%', left: '30.8%', transform: 'rotate(270deg)' },
+      { top: '82.5%', left: '23.8%', transform: 'rotate(280deg)' },
+      { top: '81.5%', left: '16.8%', transform: 'rotate(290deg)' },
+      { top: '80.1%', left: '9.8%', transform: 'rotate(290deg)' },
+      { top: '78.1%', left: '3.5%', transform: 'rotate(310deg)' },
+      { top: '74.1%', left: '-1.2%', transform: 'rotate(360deg)' },
+      { top: '69.3%', left: '-0.4%', transform: 'rotate(30deg)' },
+      { top: '65.3%', left: '2.9%', transform: 'rotate(45deg)' },
+      { top: '61.8%', left: '7.8%', transform: 'rotate(65deg)' },
+      { top: '59.8%', left: '14.7%', transform: 'rotate(70deg)' },
+      { top: '58.3%', left: '21.8%', transform: 'rotate(75deg)' },
+      { top: '56.8%', left: '28.8%', transform: 'rotate(85deg)' },
+      { top: '56.3%', left: '36.3%', transform: 'rotate(90deg)' },
+      { top: '54.8%', left: '42.8%', transform: 'rotate(95deg)' },
     ];
 
     return (
